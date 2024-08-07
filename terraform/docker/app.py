7a017facbed9785aaacb7240da733c0a59e9cc2a
Jean-Pierre Gbedjissi <jeanpierrecaleb@gmail.com>
2024-08-07 17:21:17 +0100

    add dockerfile cmt-01

--- terraform/docker/app.py
+++ terraform/docker/app.py
@@ -1,10 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return 'Hello, Docker is here for you !'
+def home():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return "This is a Flask web application."
+
+@app.route('/contact')
+def contact():
+    return "Contact us at: contact@flask.com"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
